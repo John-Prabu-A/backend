4195fbdb059435a9bbffa79c7bcce1f3432e5f18
John-Prabu-A <johnprabu0702@gmail.com>
2025-04-09 22:14:46 +0530

    fix: auth forbidden issue fixed

--- src/main/java/com/jpdevland/foodyheaven/backend/FoodyHeavenApplication.java
+++ src/main/java/com/jpdevland/foodyheaven/backend/FoodyHeavenApplication.java
@@ -2,8 +2,13 @@ package com.jpdevland.foodyheaven.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
+
+import com.jpdevland.foodyheaven.backend.config.ApplicationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(ApplicationProperties.class)
 public class FoodyHeavenApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/jpdevland/foodyheaven/backend/model/Role.java
+++ src/main/java/com/jpdevland/foodyheaven/backend/model/Role.java
@@ -2,5 +2,5 @@ package com.jpdevland.foodyheaven.backend.model;
 
 public enum Role {
     ROLE_USER,
-    ROLE_ADMIN
+ROLE_ADMIN,
 }
